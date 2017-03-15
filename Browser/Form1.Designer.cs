e01602a6c68b22e1f48c6b2f4f1fb89f768af087
john.wang <john.wang@advantech.com.tw>
2017-03-15 17:21:04 +0800

    First init

---
+++ Browser/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Browser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改這個方法的內容。
+        ///
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser3 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser4 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(638, 455);
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser2.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(638, 455);
+            // 
+            // webBrowser3
+            // 
+            this.webBrowser3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser3.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser3.Name = "webBrowser3";
+            this.webBrowser3.Size = new System.Drawing.Size(638, 455);
+            // 
+            // webBrowser4
+            // 
+            this.webBrowser4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser4.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser4.Name = "webBrowser4";
+            this.webBrowser4.Size = new System.Drawing.Size(638, 455);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(638, 455);
+            this.Controls.Add(this.webBrowser4);
+            this.Controls.Add(this.webBrowser3);
+            this.Controls.Add(this.webBrowser2);
+            this.Controls.Add(this.webBrowser1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.WebBrowser webBrowser2;
+        private System.Windows.Forms.WebBrowser webBrowser3;
+        private System.Windows.Forms.WebBrowser webBrowser4;
+    }
+}
+
---
+++ Browser/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Browser
+{
+
+    enum Page 
+    {
+        Page1,
+        Page2,
+        Page3,
+        Page4
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+            try
+            {
+                setPage(Page.Page1);
+                webBrowser1.Navigate(new Uri("http://www.google.com"));
+                webBrowser2.Navigate(new Uri("http://www.yahoo.com"));
+                webBrowser3.Navigate(new Uri("http://www.google.com"));
+                webBrowser4.Navigate(new Uri("http://www.youtube.com"));
+                this.KeyPreview = true;
+                this.KeyDown += new KeyEventHandler(Form_KeyDown);
+                
+            }
+            catch (Exception e) 
+            {
+
+            }
+
+            #region Auto scale layout setting
+
+            int formHeight = Screen.PrimaryScreen.Bounds.Height;
+            int formWidth = Screen.PrimaryScreen.Bounds.Width;
+
+            this.Height = formHeight;
+            this.Width = formWidth;
+
+            double widthbase, heightbase;
+            this.Tag = 452 + "|" + 795;
+            widthbase = formWidth / double.Parse(this.Tag.ToString().Split('|')[1]);
+            heightbase = formHeight / double.Parse(this.Tag.ToString().Split('|')[0]);
+
+            foreach (Control o in this.Controls)
+            {
+                o.Tag = o.Top + "|" + o.Left + "|" + o.Height + "|" + o.Width;
+                o.Width = (int)(double.Parse(o.Tag.ToString().Split('|')[3]) * widthbase);
+                o.Height = (int)(double.Parse(o.Tag.ToString().Split('|')[2]) * heightbase);
+                o.Left = (int)(double.Parse(o.Tag.ToString().Split('|')[1]) * widthbase);
+                o.Top = (int)(double.Parse(o.Tag.ToString().Split('|')[0]) * heightbase);
+            }
+
+            webBrowser1.Location = new System.Drawing.Point(formWidth / 2 - 200, formHeight / 2);
+
+            #endregion
+        }
+
+        private void setPage(Page page)
+        {
+            webBrowser1.Visible = false;
+            webBrowser2.Visible = false;
+            webBrowser3.Visible = false;
+            webBrowser4.Visible = false;
+            switch (page) 
+            {
+                case Page.Page1:
+                    webBrowser1.Visible = true;
+                    break;
+                case Page.Page2:
+                    webBrowser2.Visible = true;
+                    break;
+                case Page.Page3:
+                    webBrowser3.Visible = true;
+                    break;
+                case Page.Page4:
+                    webBrowser4.Visible = true;
+                    break;
+            }
+        }
+
+        void Form_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+            
+            if (e.KeyCode == Keys.Escape) 
+            {
+                this.Close();
+            }
+
+
+            if (e.Control) 
+            {
+                switch(e.KeyCode){
+                    case Keys.D1:
+                        setPage(Page.Page1);
+                        break;
+                    case Keys.D2:
+                        setPage(Page.Page2);
+                        break;
+                    case Keys.D3:
+                        setPage(Page.Page3);
+                        break;
+                    case Keys.D4:
+                        setPage(Page.Page4);
+                        break;
+                }
+            }
+        }
+
+    }
+}
